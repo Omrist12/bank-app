10d56ef664a579d158407491241b1fefbe5697e7
Omri Steinberg <omrist10@gmail.com>
2025-06-11 14:13:15 +0300

    feat: implement phone verification via /verify route (issue #7)

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./docs/swagger.yaml');
-
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-const signupRoute = require('./routes/signup');
-app.use('/signup', signupRoute);
+const { router: signupRoute } = require("./routes/signup");
+const verifyRoute = require("./routes/verify");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/signup", signupRoute);
+app.use("/verify", verifyRoute);
 
 // Test route
-app.get('/', (req, res) => {
-  res.send('Bank API is running...');
+app.get("/", (req, res) => {
+  res.send("Bank API is running...");
 });
 
 module.exports = app;
